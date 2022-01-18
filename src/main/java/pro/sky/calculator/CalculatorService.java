bf7189d6676e56646d05f562c258636054c20159
SergeFox02 <Fox2188@gmail.com>
2022-01-19 00:03:09 +0300

    Второй коммит. Выполнил домашнее задание.

---
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -0,0 +1,9 @@
+package pro.sky.calculator;
+
+public interface CalculatorService {
+    String showHelloCalculator();
+    String sum(int num1, int num2);
+    String difference(int num1, int num2);
+    String multiply(int num1, int num2);
+    String divide(int num1, int num2);
+}
---
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -0,0 +1,31 @@
+package pro.sky.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService{
+    @Override
+    public String showHelloCalculator() {
+        return "Добро пожаловать в калькулятор!";
+    }
+
+    @Override
+    public String sum(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    @Override
+    public String difference(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    @Override
+    public String multiply(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    @Override
+    public String divide(int num1, int num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+}
